f171ac7ff7bcfd00a089121f5c57024d741d4dc4
prifrack <kevindaza071@gmail.com>
2024-05-20 22:40:04 -0500

    Importé el módulo JWT en el authmodule y lo configuŕe con las variables de entorno, además creé un .env.example para la facilidad de uso del trainer

--- src/libs/auth/auth.module.ts
+++ src/libs/auth/auth.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './controllers/auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './services/auth.service';
+import { UtilsModule } from 'libs/utils/utils.module';
+import { UsersModule } from 'modules/users/users.module';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
-@Module({})
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.ACCESS_TOKEN_EXPIRY || '15m' },
+    }),
+    UtilsModule,
+    UsersModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+})
 export class AuthModule {}
