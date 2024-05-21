88b79759854020ae355987a409d1bc2df9733c9a
prifrack <kevindaza071@gmail.com>
2024-05-20 22:23:34 -0500

    creé decoradores personales para validar la privacidad de la endpoint, creé el guardián y lo agregué al controlador

--- src/libs/auth/controllers/auth.controller.ts
+++ src/libs/auth/controllers/auth.controller.ts
@@ -8,16 +8,16 @@ import {
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { SignUpDto, UserLoginDto } from '../dtos';
-//   import { AtGuard } from '../guards/at.guard';
-//   import { Public } from 'src/libs/decorators';
+import { MyGuard } from '../guards/auth.guard';
 import { AuthService } from '../services/auth.service';
+import { Public } from 'libs/decorators';
 
 @ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-//   @Public()
+  @Public()
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
   async register(@Body() signUpDto: SignUpDto) {
@@ -25,7 +25,7 @@ export class AuthController {
     return { access_token: token.access_token };
   }
 
-//   @Public()
+  @Public()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   async logIn(@Body() userLogInDto: UserLoginDto) {
@@ -34,11 +34,11 @@ export class AuthController {
     return { access_token: token.access_token };
   }
 
-  // @Post('check')
-  // @UseGuards(AtGuard)
-  // @ApiBearerAuth()
-  // @HttpCode(HttpStatus.OK)
-  // async check() {
-  //   return true;
-  // }
+  @Post('check')
+  @UseGuards(MyGuard)
+  @ApiBearerAuth()
+  @HttpCode(HttpStatus.OK)
+  async check() {
+    return true;
+  }
 }
